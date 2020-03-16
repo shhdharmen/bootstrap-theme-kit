ef5f14cafb1c037e56e4b72d27bb23261aace4a6
Dharmen Shah <dharmen.s@milestoneinternet.com>
2020-03-16 15:42:58 +0530

    fix(theme): loading file name from manifest file

--- src/main.js
+++ src/main.js
@@ -1,22 +1,39 @@
 const LOCAL_STORAGE_KEY = "bootstrap-theme-kit";
 const meta = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-// file path is same as what we've set in src\assets\rev-manifest.json
-const DARK_THEME_PATH = "css/style-dark-00bee02303.css";
 
+let DARK_THEME_PATH;
 let isDark = meta && meta.isDark;
 let darkStyleLink = document.getElementById("dark-theme-style");
 let themeToggle = document.getElementById("theme-toggle");
+let jsonFetched = false;
 
+fetch("./assets/rev-manifest.json")
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    jsonFetched = true;
+    // file path is same as what we've set in src\assets\rev-manifest.json
+    DARK_THEME_PATH = data["css/style-dark.css"];
+    // check on doc load
+    if (isDark) {
+      showDarkTheme();
+    } else {
+      hideDarkTheme();
+    }
+  });
 // eslint-disable-next-line no-unused-vars
 function toggleDark() {
-  isDark = !isDark;
-  if (isDark) {
-    showDarkTheme();
-  } else {
-    hideDarkTheme();
+  if (jsonFetched) {
+    isDark = !isDark;
+    if (isDark) {
+      showDarkTheme();
+    } else {
+      hideDarkTheme();
+    }
+    let meta = { isDark };
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(meta));
   }
-  let meta = { isDark };
-  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(meta));
 }
 
 function showDarkTheme() {
@@ -26,11 +43,4 @@ function showDarkTheme() {
 function hideDarkTheme() {
   darkStyleLink.setAttribute("href", "");
   themeToggle.innerHTML = "🌞 Light";
-}
-
-// check on doc load
-if (isDark) {
-  showDarkTheme();
-} else {
-  hideDarkTheme();
 }
